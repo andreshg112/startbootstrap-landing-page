a25386acdff0d4f825b820be54566e8f7416178e
andreshg112 <andreshg112@gmail.com>
2016-09-09 20:16:17 -0500

    Se agregó Analytics.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//Google analytics
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-83946523-1', 'auto');
+ga('send', 'pageview');
+
+
 $(document).on('click', '.navbar-collapse.in', function (e) {
     if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
         $(this).collapse('hide');
